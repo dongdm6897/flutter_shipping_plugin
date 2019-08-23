e5decf79c2043fbe98bf0cb96f102b844e3fc27b
manhdong6897 <manhdong6897@gmail.com>
2019-08-23 16:14:17 +0700

    fix pop screen

--- lib/src/ui/shipping_provider_list..dart
+++ lib/src/ui/shipping_provider_list..dart
@@ -18,84 +18,84 @@ class _ShippingProviderListState extends State<ShippingProviderList> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    /*return MaterialApp(
       home: _shipProviderList(),
       routes: <String, WidgetBuilder>{
         '/screen1': (BuildContext context) => SuperShipReferencePriceList(),
         '/screen2': (BuildContext context) => GhnReferencePriceList(),
       },
-    );
-  }
-
-  Widget _shipProviderList() {
-    return Scaffold(
-      body: SimpleDialog(
-        children: <Widget>[
-          Container(
-            width: MediaQuery.of(context).size.width * 0.9,
-            child: FutureBuilder(
-              future: ShippingListApiProvider().getListShipProvider(),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if (snapshot.hasData) {
-                  shipProvider = snapshot.data;
-                  return Row(
-                    children: <Widget>[
-                      new Flexible(
-                          child: Column(
-                        children: <Widget>[
-                          Text(
-                            "Ship provider",
-                            style: new TextStyle(
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black),
-                          ),
-                          Divider(),
-                          Container(
-                            padding: EdgeInsets.all(10.0),
-                            height: shipProvider.length * 60.0,
-                            child: ListView.builder(
-                                itemCount: shipProvider.length,
-                                itemBuilder: (BuildContext context, int idx) {
-                                  var item = shipProvider[idx];
-                                  return ListTile(
-                                    contentPadding: EdgeInsets.all(0.0),
-                                    title: Column(
-                                      children: <Widget>[
-                                        Row(children: <Widget>[
-                                          Padding(padding: EdgeInsets.all(5.0)),
-                                          Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: <Widget>[
-                                              Text(item.name),
-                                            ],
-                                          )
-                                        ]),
-                                        Divider()
-                                      ],
-                                    ),
-                                    trailing: GestureDetector(
+    );*/
+    return SimpleDialog(
+      children: <Widget>[
+        Container(
+          width: MediaQuery.of(context).size.width * 0.9,
+          child: FutureBuilder(
+            future: ShippingListApiProvider().getListShipProvider(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                shipProvider = snapshot.data;
+                return Row(
+                  children: <Widget>[
+                    new Flexible(
+                        child: Column(
+                      children: <Widget>[
+                        Text(
+                          "Ship provider",
+                          style: new TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                        ),
+                        Divider(),
+                        Container(
+                          padding: EdgeInsets.all(10.0),
+                          height: shipProvider.length * 60.0,
+                          child: ListView.builder(
+                              itemCount: shipProvider.length,
+                              itemBuilder: (BuildContext context, int idx) {
+                                var item = shipProvider[idx];
+                                return ListTile(
+                                  contentPadding: EdgeInsets.all(0.0),
+                                  title: Column(
+                                    children: <Widget>[
+                                      Row(children: <Widget>[
+                                        Padding(padding: EdgeInsets.all(5.0)),
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: <Widget>[
+                                            Text(item.name),
+                                          ],
+                                        )
+                                      ]),
+                                      Divider()
+                                    ],
+                                  ),
+                                  trailing: GestureDetector(
                                       child: Icon(Icons.airline_seat_flat),
                                       onTap: () {
-                                        showDialog(context: context,
-                                        builder: (BuildContext context){
-                                          switch(item.id){
-                                            case ShipProviderEnum.SUPER_SHIP :
-                                              return SuperShipReferencePriceList();
-                                              break;
-                                            case ShipProviderEnum.GHN :
-                                              return GhnReferencePriceList();
-                                              break;
-                                            default :
-                                              return null;
-                                              break;
-                                          }
 
-                                        });
+                                        switch (item.id) {
+                                          case ShipProviderEnum.SUPER_SHIP:
+                                            print("fuck");
+                                            Navigator.of(context).push(
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        SuperShipReferencePriceList()));
+                                            break;
+                                          case ShipProviderEnum.GHN:
+                                            Navigator.of(context).push(
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        GhnReferencePriceList()));
+                                            break;
+                                          default:
+                                            return null;
+                                            break;
+                                        }
                                       }),
-                                    onTap: () {
-                                      /*showDialog(
+                                  onTap: () {
+                                    /*showDialog(
                                           context: context,
                                           builder: (BuildContext context) {
                                             return SimpleDialog(
@@ -104,22 +104,117 @@ class _ShippingProviderListState extends State<ShippingProviderList> {
                                               ],
                                             );
                                           });*/
-                                      Navigator.pop(context, item);
-                                    },
-                                  );
-                                }),
-                          ),
-                        ],
-                      ))
-                    ],
-                  );
-                } else
-                  return Text("Loading....");
-              },
-            ),
-          )
-        ],
-      ),
+                                    Navigator.pop(context, item);
+                                  },
+                                );
+                              }),
+                        ),
+                      ],
+                    ))
+                  ],
+                );
+              } else
+                return Text("Loading....");
+            },
+          ),
+        )
+      ],
     );
   }
+
+//  Widget _shipProviderList() {
+//    return Scaffold(
+//      body: SimpleDialog(
+//        children: <Widget>[
+//          Container(
+//            width: MediaQuery.of(context).size.width * 0.9,
+//            child: FutureBuilder(
+//              future: ShippingListApiProvider().getListShipProvider(),
+//              builder: (BuildContext context, AsyncSnapshot snapshot) {
+//                if (snapshot.hasData) {
+//                  shipProvider = snapshot.data;
+//                  return Row(
+//                    children: <Widget>[
+//                      new Flexible(
+//                          child: Column(
+//                        children: <Widget>[
+//                          Text(
+//                            "Ship provider",
+//                            style: new TextStyle(
+//                                fontSize: 18.0,
+//                                fontWeight: FontWeight.bold,
+//                                color: Colors.black),
+//                          ),
+//                          Divider(),
+//                          Container(
+//                            padding: EdgeInsets.all(10.0),
+//                            height: shipProvider.length * 60.0,
+//                            child: ListView.builder(
+//                                itemCount: shipProvider.length,
+//                                itemBuilder: (BuildContext context, int idx) {
+//                                  var item = shipProvider[idx];
+//                                  return ListTile(
+//                                    contentPadding: EdgeInsets.all(0.0),
+//                                    title: Column(
+//                                      children: <Widget>[
+//                                        Row(children: <Widget>[
+//                                          Padding(padding: EdgeInsets.all(5.0)),
+//                                          Column(
+//                                            crossAxisAlignment:
+//                                                CrossAxisAlignment.start,
+//                                            children: <Widget>[
+//                                              Text(item.name),
+//                                            ],
+//                                          )
+//                                        ]),
+//                                        Divider()
+//                                      ],
+//                                    ),
+//                                    trailing: GestureDetector(
+//                                      child: Icon(Icons.airline_seat_flat),
+//                                      onTap: () {
+//                                        showDialog(context: context,
+//                                        builder: (BuildContext context){
+//                                          switch(item.id){
+//                                            case ShipProviderEnum.SUPER_SHIP :
+//                                              return SuperShipReferencePriceList();
+//                                              break;
+//                                            case ShipProviderEnum.GHN :
+//                                              return GhnReferencePriceList();
+//                                              break;
+//                                            default :
+//                                              return null;
+//                                              break;
+//                                          }
+//
+//                                        });
+//                                      }),
+//                                    onTap: () {
+//                                      /*showDialog(
+//                                          context: context,
+//                                          builder: (BuildContext context) {
+//                                            return SimpleDialog(
+//                                              children: <Widget>[
+//                                                Image.network(item.imageFee)
+//                                              ],
+//                                            );
+//                                          });*/
+//                                      Navigator.pop(context, item);
+//                                    },
+//                                  );
+//                                }),
+//                          ),
+//                        ],
+//                      ))
+//                    ],
+//                  );
+//                } else
+//                  return Text("Loading....");
+//              },
+//            ),
+//          )
+//        ],
+//      ),
+//    );
+//  }
 }
