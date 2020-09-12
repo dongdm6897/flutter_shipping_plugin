aed2cf45bca5f4861938e00476b630adb2df5d2c
Nguyen Dat <nguyendat.5997@gmail.com>
2020-09-12 18:37:55 +0700

    Fix text overflow

--- lib/src/ui/widgets/shipping_provide_list.dart
+++ lib/src/ui/widgets/shipping_provide_list.dart
@@ -30,74 +30,74 @@ class _ShipProviderList extends State<ShipProviderList> {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return SimpleDialog(
-        contentPadding: EdgeInsets.all(10.0),
+    return SimpleDialog(contentPadding: EdgeInsets.all(10.0), children: <
+        Widget>[
+      new Row(
         children: <Widget>[
-          new Row(
+          new Flexible(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              new Flexible(
-                  child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      "Danh sách đơn vận chuyển",
-                      style: TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black87),
-                    ),
-                  ),
-                  Divider(),
-                  Container(
-                    padding: EdgeInsets.all(10.0),
-                    width: size.width * 0.9,
-                    height: _listShipProvider.length * 70.0,
-                    child: ListView.builder(
-                        itemCount: _listShipProvider.length,
-                        itemBuilder: (BuildContext context, int idx) {
-                          var item = _listShipProvider[idx];
-                          return ListTile(
-                            contentPadding: EdgeInsets.all(0.0),
-                            title: Column(
-                              children: <Widget>[
-                                Row(children: <Widget>[
-                                  CircleAvatar(
-                                      child: Image.network(
-                                    item.logo,
-                                    height: 100.0,
-                                    width: 100.0,
-                                  )), //item.logo
-                                  Padding(padding: EdgeInsets.all(5.0)),
-                                  Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: <Widget>[
-                                      Text(item.name,
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold)),
-                                      Text(
-                                        item.description ?? "Đơn vị vận chuyển",
-                                        style: TextStyle(color: Colors.grey),
-                                      ),
-                                    ],
-                                  )
-                                ]),
-                                Divider()
-                              ],
-                            ),
-                            onTap: () async {
-                              Navigator.pop(context, item);
-                            },
-                          );
-                        }),
-                  ),
-                ],
-              ))
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Danh sách đơn vận chuyển",
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87),
+                ),
+              ),
+              Divider(),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                width: size.width * 0.9,
+                height: _listShipProvider.length * 70.0,
+                child: ListView.builder(
+                    itemCount: _listShipProvider.length,
+                    itemBuilder: (BuildContext context, int idx) {
+                      var item = _listShipProvider[idx];
+                      return ListTile(
+                        contentPadding: EdgeInsets.all(0.0),
+                        title: Column(
+                          children: <Widget>[
+                            Row(children: <Widget>[
+                              CircleAvatar(
+                                  child: Image.network(
+                                item.logo,
+                                height: 100.0,
+                                width: 100.0,
+                              )), //item.logo
+                              Padding(padding: EdgeInsets.all(5.0)),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Text(item.name,
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold)),
+                                    Text(
+                                      item.description ?? "Đơn vị vận chuyển",
+                                      style: TextStyle(color: Colors.grey),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ]),
+                            Divider()
+                          ],
+                        ),
+                        onTap: () async {
+                          Navigator.pop(context, item);
+                        },
+                      );
+                    }),
+              ),
             ],
-          )
-        ]);
+          ))
+        ],
+      )
+    ]);
   }
 }
