d2f04c2c0c6ad9128f1f7eaf65e5e46ae06b78c8
Nguyen Dat <nguyendat.5997@gmail.com>
2021-05-21 10:53:06 +0700

    Remove folder not used

--- test/shipping_plugin_test.dart
+++ test/shipping_plugin_test.dart
@@ -1,20 +1,5 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('shipping_plugin');
-
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-//    expect(await ShippingPlugin.platformVersion, '42');
-  });
+  test('adds one to input values', () {});
 }
